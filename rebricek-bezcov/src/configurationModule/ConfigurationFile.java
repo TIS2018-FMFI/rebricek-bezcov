d465262ea1aa2de2e5d0f881cac5379a85364771
Jozko <j.mizerik@gmail.com>
2018-11-17 20:51:01 +0100

    Konfiguračný modul hotový

---
+++ rebricek-bezcov/src/configurationModule/ConfigurationFile.java
@@ -0,0 +1,68 @@
+package configurationModule;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ConfigurationFile {
+
+	private int seasonYear, rankedRounds, pointingType, constant;
+	private int[] pointingList;
+	
+	public ConfigurationFile(File configFile) {
+		try {
+			Scanner scanner = new Scanner(configFile);
+			seasonYear = takeIntegerInLine(scanner.nextLine());
+			rankedRounds = takeIntegerInLine(scanner.nextLine());
+			pointingType = takeIntegerInLine(scanner.nextLine());
+			if(pointingType == 1) {
+				String line = scanner.nextLine();
+				int index = line.indexOf("//");
+				line = line.substring(0, index).trim();
+				String[] parts = line.split(",");
+				pointingList = Arrays.stream(parts).mapToInt(Integer::parseInt).toArray();
+			}
+			else if(pointingType == 3) {
+				scanner.nextLine();
+				constant = takeIntegerInLine(scanner.nextLine());
+				}
+			scanner.close();
+			}
+			catch(IOException e) {
+				System.out.println(e);
+			}
+	}
+	
+	
+	private int takeIntegerInLine(String line) {
+		int index = line.indexOf("//");
+		return Integer.parseInt(line.substring(0, index).trim());
+	}
+
+
+	public int getSeasonYear() {
+		return seasonYear;
+	}
+
+
+	public int getRankedRounds() {
+		return rankedRounds;
+	}
+
+
+	public int getPointingType() {
+		return pointingType;
+	}
+
+
+	public int[] getPointingList() {
+		return pointingList;
+	}
+
+
+	public int getConstant() {
+		return constant;
+	}
+	
+}
